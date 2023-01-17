c17c9c2992122444dba1ffc44ac52085e427eb2e
adrianbelend <5730@holbertonstudents.com>
2023-01-17 09:32:24 -0800

    task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pow(a, b):
+    c=1
+    d=b
+    if b == 0:
+        return 1
+    if b < 0:
+        b = b*-1
+    for i in range(0,b):
+        c=c*a
+    if d < 0:
+        c = 1/c
+    return (c)
