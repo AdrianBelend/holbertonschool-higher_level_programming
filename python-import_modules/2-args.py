e4d5e82b5c86d256e748cfce4144da3869453b79
adrianbelend <5730@holbertonstudents.com>
2023-01-18 08:07:15 -0800

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
 a = 0
+print("{} arguments:".format(len(argv) - 1))
+
 for i in range(1, len(argv)):
     a = a + 1
     print("{}: {}".format(a, argv[i]))
