c97bb2d77fab5349d563f805bd929a9c8dec2f58
adrianbelend <5730@holbertonstudents.com>
2023-01-18 08:12:32 -0800

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
 a = 0
-print("{} arguments:".format(len(argv) - 1))
+if len(argv) == 2:
+    print ("1 argument:")
+else:
+    print("{} arguments:".format(len(argv) - 1))
 
 for i in range(1, len(argv)):
     a = a + 1
