645cffeead47ceea1f86867b5ef2b65bb233fe41
Adrianbelend <5730@holbertonstudents.com>
2023-01-21 10:01:19 -0800

    advanced 2

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     abc = ""
-    for i in range(0,len(str)):
+    for i in range(0, len(str)):
         if i == n:
             continue
         else:
