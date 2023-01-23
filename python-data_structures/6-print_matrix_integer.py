2512bafffbaf9fef6de069dca02697ec28fd10a6
Adrianbelend <5730@holbertonstudents.com>
2023-01-23 07:32:57 -0800

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(0, len(matrix)):
+        for a in range(0, (len(matrix[i]))):
+            print("{}".format(matrix[i][a]), end="")
+            if a != len(matrix[i]) - 1:
+                print(" ", end="")
+        print()
