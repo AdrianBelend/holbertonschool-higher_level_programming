8a91f1439f70c5bc6516b0a9e0723d087b44123c
Adrianbelend <5730@holbertonstudents.com>
2023-01-23 09:28:37 -0800

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,14 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     str = [0, 0]
     if len(tuple_a) < 2:
-        tuple_c = tuple_a + (0, 0)
-        for i in range(2):
-            str[i] = tuple_c[i] + tuple_b[i]
-    elif len(tuple_b) < 2:
-        tuple_c = tuple_b + (0, 0)
-        for i in range(2):
-            str[i] = tuple_a[i] + tuple_c[i]
-    else:
-        for i in range(2):
-            str[i] = tuple_a[i] + tuple_b[i]
-    return(str)
+        tuple_a += (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_a[1])
