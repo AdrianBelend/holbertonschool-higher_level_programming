5815763abb631fab5201d1fc615dcdb8a4fbb8b5
Adrianbelend <5730@holbertonstudents.com>
2023-01-23 06:47:45 -0800

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     a = 0
-    new_string = ''
+    new_string = my_string
     for i in range(0, len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            new_string = my_string[0: i - a] + my_string[(i + 1):]
+            new_string = new_string[0: (i - a)] + my_string[(i + 1):]
             a = a + 1
     return(new_string)
