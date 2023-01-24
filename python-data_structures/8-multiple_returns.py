020672d845b9f1e672fd2593713f60663c72ce6d
Adrianebelend <5730@holbertonstudents.com>
2023-01-24 07:32:21 -0800

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == []:
-        tuple = (len(sentence), None)
+    if sentence == "":
+        tuple = (0, None)
     else:
         tuple = (len(sentence), sentence[0])
     return(tuple)
