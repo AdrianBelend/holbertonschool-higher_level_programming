a9c9818797c9afbae5b4c8b82af1d7eff5119262
Adrianebelend <5730@holbertonstudents.com>
2023-01-25 08:29:08 -0800

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [0] * len(matrix) 
+    new_matrix = [0] * len(matrix)
     new_list = [0] * len(matrix[0])
     for i in range(len(matrix)):
         for b in range(len(matrix[i])):
