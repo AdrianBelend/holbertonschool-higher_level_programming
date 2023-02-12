51470128175ca7c85c53ec07211266d00dcfa3e0
Adrianbelend <5730@holbertonstudents.com>
2023-02-12 11:36:18 -0800

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """returns True if the object is an instance of the class"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """returns True if the object is an instance of the specified class"""
     if isinstance(obj, a_class):
         return True
