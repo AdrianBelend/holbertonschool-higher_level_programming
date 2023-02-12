e2297a6624ae97801f8fce65c22120058bc66457
Adrianbelend <5730@holbertonstudents.com>
2023-02-12 11:59:33 -0800

    task 6

---
+++ python-inheritance/tests/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """exception"""
+        raise Exception("area() is not implemented")
