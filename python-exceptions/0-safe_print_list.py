9f101cc33a3da0e8711d0c8a1b0f19273c588537
AdrianBelend <5730@holbertonstudents.com>
2023-06-08 06:31:01 -0700

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for a in range(x):
+    i = 0
+    for a in range(x):
+        try:
             print("{}".format(my_list[a]), end="")
-        print("")
-        return (a + 1)
-    except:
-        print("")
-        return a
+            i += 1
+        except:
+            print("")
+            return a
+    print("")
+    return i
