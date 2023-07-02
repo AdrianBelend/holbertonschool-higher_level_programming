4c6f6ba8f940317c9fcb41ebff93106e90693eba
Adrianbelend <5730@holbertonstudents.com>
2023-07-02 16:38:16 -0700

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,20 @@ class Rectangle(Base):
             for d in range(self.__width):
                 print("#", end="")
             print()
+
+    def update(self, *args, **kwargs):
+        """update"""
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.__width = args[1]
+            if len(args) == 3:
+                self.__height = args[2]
+            if len(args) == 4:
+                self.__x = args[3]
+            if len(args) == 5:
+                self.__y = args[4]
+            elif kwargs:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
